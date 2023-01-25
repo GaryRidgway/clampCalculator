7bcfb92f081d5dcee28ef1e4de562c3f6d2c62a4
Gary Ridgway <gary-ridgway@uiowa.edu>
2023-01-25 10:48:41 -0600

    Lots of refactoring.

--- js/calc.js
+++ js/calc.js
@@ -11,63 +11,143 @@ document.addEventListener('DOMContentLoaded', function () {
     replace(element);
   });
 
-  // If we have all the right entries, declare a yeehaw.
-  if (
-    paramKeys.indexOf('minWidth') >= 0 &&
-    paramKeys.indexOf('maxWidth') >= 0 &&
-    paramKeys.indexOf('minFont') >= 0 &&
-    paramKeys.indexOf('maxFont') >= 0
-  ) {
-    console.log ('yeehaw.');
-    // And calculate the rule.
-    calcRule();
+  // And calculate the rule.
+  let userAuto = false;
+  if (paramKeys.indexOf('auto') >= 0) {
+    userAuto = true;
   }
+
+  calcRule(true, false, userAuto);
 }, false);
 
-function calcSlope() {
+function getReferences() {
   let minWidth = document.getElementById('minWidth').value;
   let maxWidth = document.getElementById('maxWidth').value;
-  let minfont_size = document.getElementById('minFont').value * remFactor;
-  let maxfont_size = document.getElementById('maxFont').value * remFactor;
+  let minFontSize = document.getElementById('minFontSize').value;
+  let maxFontSize = document.getElementById('maxFontSize').value;
+  let minFontSizePix = minFontSize * remFactor;
+  let maxFontSizePix = maxFontSize * remFactor;
 
-  let slope = (maxfont_size - minfont_size)/(maxWidth - minWidth);
+  let refs = {
+    minWidth:       minWidth,
+    maxWidth:       maxWidth,
+    minFontSize:    minFontSize,
+    maxFontSize:    maxFontSize,
+    minFontSizePix: minFontSizePix,
+    maxFontSizePix: maxFontSizePix
+  };
+
+  return refs;
+}
+
+function calcRule(auto=false, doAlert = true, userAuto = false) {
+  const refs = getReferences();
+
+  // !!!!
+  if (
+    auto &&
+    !(
+      paramKeys.indexOf('minWidth') >= 0 &&
+      paramKeys.indexOf('minWidth') >= 0 &&
+      paramKeys.indexOf('maxWidth') >= 0 &&
+      paramKeys.indexOf('maxWidth') >= 0 &&
+      urlParams.get('minFontSize') !== '' &&
+      urlParams.get('minFontSize') !== '' &&
+      urlParams.get('maxFontSize') !== '' &&
+      urlParams.get('maxFontSize') !== ''
+    )
+  ) {
+    return;
+  }
+
+  // !!!!!
+  let valid = true;
+  Object.keys(refs).forEach((element) => {
+    const val = refs[element];
+    const valueExists = (val !== '' && val !== 0);
+
+    valid = valid && valueExists
+  });
+
+  if (
+    !auto && !valid
+  ) {
+    if(doAlert) {
+      swal(
+        'Incorrect values',
+        'The values entered are not complete, please ensure you have filled them all out correctly.',
+        'error'
+      );
+    }
+
+    return;
+  }
+
+  // And calculate the rule.
+  const slope = calcSlope(refs.minWidth, refs.maxWidth, refs.minFontSizePix, refs.maxFontSizePix);
+  const slopeAtWidth = Math.round(slope*100 * 10000) / 10000;
+  const intercept = calcIntercept(slope, refs.minWidth, refs.minFontSizePix);
+  const rIntercept = Math.round(intercept * 10000) / 10000;
+  const operator = (intercept<0) ? '-' : '+';
+
+  const prefVal = 'calc(' + slopeAtWidth + 'vw ' + operator + ' ' + Math.abs(rIntercept) +'rem)';
+  const minFontVal = refs.minFontSize;
+  const maxFontVal = refs.maxFontSize;
+
+  const clampRule =  'clamp(' + minFontVal + 'rem, ' + prefVal + ', ' + maxFontVal + 'rem);';
+
+  document.getElementById('output').innerHTML = clampRule;
+
+  if (!auto || userAuto) {
+    // https://stackoverflow.com/questions/56306153/domexception-on-calling-navigator-clipboard-readtext
+    navigator.clipboard.writeText(clampRule).then(function(x) {
+      const rule = document.createElement('div');
+      rule.classList.add('preformatted');
+      rule.textContent = clampRule;
+      swal({
+        title: 'Clamp rule copied to clipboard',
+        content: rule,
+        icon: 'success'
+      });
+    });
+  }
+
+  const refKeys = Object.keys(refs);
+
+  let queryString = 
+    '/?' + refKeys[0] + '=' + refs[refKeys[0]] + '&' +
+    refKeys[1] + '=' + refs[refKeys[1]] + '&' +
+    refKeys[2] + '=' + refs[refKeys[2]] + '&' +
+    refKeys[3] + '=' + refs[refKeys[3]]
+  ;
+
+  if (paramKeys.indexOf('auto') >= 0 && (userAuto || !auto)) {
+    queryString += '&auto';
+  }
+
+  window.history.pushState('', '', queryString);
+}
+
+function calcSlope(minWidth, maxWidth, minFontSize, maxFontSize) {
+  let slope = (maxFontSize - minFontSize)/(maxWidth - minWidth);
 
   return slope;
 }
 
-function calcIntercept() {
-  let minWidth = document.getElementById('minWidth').value;
-  let maxWidth = document.getElementById('maxWidth').value;
-  let minfont_size = document.getElementById('minFont').value * remFactor;
-  let maxfont_size = document.getElementById('maxFont').value * remFactor;
-
-  let slope = calcSlope();
-  let intercept = (minfont_size - (slope * minWidth)) / remFactor;
+function calcIntercept(slope, minWidth, minFontSize) {
+  let intercept = (minFontSize - (slope * minWidth)) / remFactor;
 
   return intercept;
 }
 
-function calcRule() {
-  let slopeAtWidth = Math.round(calcSlope()*100 * 10000) / 10000;
-  let intercept = Math.round(calcIntercept() * 10000) / 10000;
-  let operator = (intercept<0) ? '-' : '+';
 
-  let prefVal = 'calc(' + slopeAtWidth + 'vw ' + operator + ' ' + Math.abs(intercept) +'rem)';
-  let minVal = document.getElementById('minFont').value;
-  let maxVal = document.getElementById('maxFont').value;
-
-  let clampRule =  'clamp(' + minVal + 'rem, ' + prefVal + ', ' + maxVal + 'rem);';
-
-  document.getElementById('output').innerHTML = clampRule;
-  navigator.clipboard.writeText(clampRule);
-}
 
 function replace(key) {
   // Check there's nothing wonky happening.
   if (paramKeys && paramKeys.indexOf(key) >= 0) {
 
     // Check that the dom contains one of these elements, and that the element is changeable.
-    let element = document.getElementById(key);
+    const element = document.getElementById(key);
     if (element && element.getAttribute('data-js-changeable') === 'true') {
 
       // If all is well, do the thing.
